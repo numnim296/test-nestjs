302a2e0693a113010db7c3f4bba8c82d387ec613
muthita munmueang <muthita.m@link-th.com>
2023-03-02 14:40:59 +0700

    prisma-mongo

--- src/controllers/device/device.controller.ts
+++ src/controllers/device/device.controller.ts
@@ -1,11 +1,9 @@
 import { GetCurrentUserEmail } from './../../decorators/get-current-user-email.decorator';
 import { CreateDeviceDto, UpdateDeviceDto } from './../../dto/device.dto';
 import { DeviceService } from './../../services/device/device.service';
-import { Body, Controller, Get, Param, Post, Query, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { FastifyReply, FastifyRequest } from 'fastify'
-import { JwtStrategy } from '../../jwt.strategy';
-import { GetCurrentUserId } from 'src/decorators';
+import { FastifyReply } from 'fastify'
 
 @Controller('device')
 export class DeviceController {
@@ -26,13 +24,13 @@ export class DeviceController {
     @UseGuards(AuthGuard('jwt'))
     @Post('create')
     createDevice(@Body() body: CreateDeviceDto, @GetCurrentUserEmail() userEmail: string, @Res() res: FastifyReply) {
-        return this.deviceService.addDevice(body,userEmail, res)
+        return this.deviceService.addDevice(body, userEmail, res)
     }
 
     @UseGuards(AuthGuard('jwt'))
     @Post('edit')
     editDevice(@Body() body: UpdateDeviceDto, @GetCurrentUserEmail() userEmail: string, @Res() res: FastifyReply) {
-        return this.deviceService.updateDevice(body,userEmail, res)
+        return this.deviceService.updateDevice(body, userEmail, res)
     }
 
 
