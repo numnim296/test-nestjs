719a2efc38e5f6868b00d18aff77f596b97e4c82
muthita munmueang <muthita.m@link-th.com>
2024-08-28 11:00:19 +0700

    old project nest

--- src/config/config.development.ts
+++ src/config/config.development.ts
@@ -4,14 +4,14 @@ export default {
   },
   database: {
     provider: 'mysql',
-    host: 'dev-db.link-th.com',
+    host: '',
     port: 3306,
-    username: 'smart_meter',
-    password: 'eAYTKsY#1ADG3',
-    name: 'smart_meter',
+    username: '',
+    password: '',
+    name: '',
   },
   cookie: {
-    key: ['1bf79deff23a4400355703fb748d58ce5739071ee001924c6196e8022715968f'],
+    key: [''],
   },
 
 }
